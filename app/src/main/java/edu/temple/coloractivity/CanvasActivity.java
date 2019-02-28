cfb923eb80869f620253f1e40f635153c9ca04f8
John <tuf97957@temple.edu>
2019-02-27 21:35:38 -0500

    Updated Assignment 4 Version - Using 2 Activities

--- app/src/main/java/edu/temple/coloractivity/CanvasActivity.java
+++ app/src/main/java/edu/temple/coloractivity/CanvasActivity.java
@@ -1,5 +1,6 @@
 package edu.temple.coloractivity;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,10 +12,10 @@ public class CanvasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
-
+        Context context = getApplicationContext();
+        setTitle(context.getResources().getString(R.string.title2));
         Bundle extras = getIntent().getExtras();
         String colorString = extras.getString("color");
-
         View colorView = findViewById(R.id.canvas_layout);
         colorView.setBackgroundColor(Color.parseColor(colorString));
     }
--- app/src/main/java/edu/temple/coloractivity/MainActivity.java
+++ app/src/main/java/edu/temple/coloractivity/MainActivity.java
@@ -5,63 +5,40 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.content.Intent;
+import android.content.Context;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Context context = getApplicationContext();
+        setTitle(context.getResources().getString(R.string.title1));
+        final Intent startCanvas = new Intent(this, CanvasActivity.class);
+        final String[] colors = context.getResources().getStringArray(R.array.colorparse);  //array of color values in format #XXXXXX
+        final String[] translated = context.getResources().getStringArray(R.array.colorlang);   //array of color descriptions localized
+        CustomAdapter colorAdapter = new CustomAdapter(this, colors, translated);
         Spinner spinner = findViewById(R.id.spinner1);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.colors, android.R.layout.simple_spinner_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-    }
+        spinner.setAdapter(colorAdapter);
+        AdapterView.OnItemSelectedListener colorListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String color = colors[position];
+                String colorString = translated[position];
+                Toast.makeText(parent.getContext(), context.getResources().getString(R.string.format) + " " + colorString, Toast.LENGTH_SHORT).show();
+                startCanvas.putExtra("color", color);
+                startActivity(startCanvas);
+            }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String color = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), "Color changed to " + color, Toast.LENGTH_SHORT).show();
-        Intent startCanvas = new Intent(this, CanvasActivity.class);
-        String colorString = "#000000";
-        switch (color) {
-            case "Red": colorString = "#FF0000";
-                break;
-            case "Green":  colorString ="#00FF00";
-                break;
-            case "Blue":  colorString = "#0000FF";
-                break;
-            case "Orange":  colorString = "#FFA500";
-                break;
-            case "Purple": colorString = "#800080";
-                break;
-            case "Cyan": colorString = "#00FFFF";
-                break;
-            case "Maroon": colorString = "#800000";
-                break;
-            case "Magenta": colorString = "#FF00FF";
-                break;
-            case "Black":  colorString = "#000000";
-                break;
-            case "White": colorString = "#FFFFFF";
-                break;
-            case "Pink": colorString = "#EE82EE";
-                break;
-            default:
-                break;
-        }
-        startCanvas.putExtra("color", colorString);
-        startActivity(startCanvas);
-    }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+            }
+        };
+        spinner.setOnItemSelectedListener(colorListener);
     }
 }
-
-
